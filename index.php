6a9b1a43bc6bb7dec7966138d0f089c1467a305b
whoaks <66693807+whoaks@users.noreply.github.com>
2020-07-05 13:46:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<style>
+body
+{
+background-color: aqua;
+margin: 2em;
+padding: 2em;
+font-size: 20px;
+font-style: italic;
+}
+#i1
+{
+background-color: black;
+color: white;
+box-shadow: 3px 3px 3px 3px red;
+}
+</style>
+</head>
+<body>
+
+Guru introduced me to myself and showed me the right way. Thanks for making me who I am. Wish you a happy Guru Purnima Day!
+
+<h1 id="i1"> Special Thanks To Vimall Daga Sir !!!!!!!!!!!!!!</h1>
+</body>
+</html>
